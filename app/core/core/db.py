ce7cfd5bba998adee3ff14e44636cea64c702e87
Guillermo Ruiz A <gui.ruiz.alvarado@gmail.com>
2021-09-13 17:25:03 -0500

    Creando proyecto y conectando a Postgre

---
+++ app/core/core/db.py
@@ -0,0 +1,12 @@
+
+
+POSTGRESQL_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
